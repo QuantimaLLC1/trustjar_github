59dccc2e6eb856e13911ca06200c903cf4cc8223
Paul Caron <paul.caron@quantimallc.com>
2015-10-20 15:23:02 -0400

    initial deployment of firebase code and other adjustments to HTML

--- js/trustjar.js
+++ js/trustjar.js
@@ -1,7 +1,184 @@
-// Initialize all tooltips
+// HOUSEKEEPING FUNCTIONS FOR PROGRAMMING PURPOSES
+  // Create a global variable array to separate our globals from the rest of the DOM.
+  GLOB = [];
+
+  // Function to reset Firebase
+   var reset = function() {
+      // Remove any previous listeners
+      GLOB.trustjarRef.off();
+      GLOB.trustjarRef.remove();
+      window.location = 'index.html';
+    }
+
+
+
+
+// CODE REQUIRED FOR UNIT TEST
+  // This allows us to run the unit test as an array of individual test components.
+  // The unit tests are in an array, allTests, defined as allTests = [ function() { ... }, function() { ... }, etc ]
+  // runAllTests iterates through the functions in the array sequentially, 
+  // and determines a pause interval between each function as specified within the unit test.
+   function runAllTests( allTests, startTestNum, endTestNum ) {
+     var returnVal = null;
+     function runAllTests1( allTests, testNum ) {
+      if( testNum == endTestNum ) {
+        return( true );
+      }
+      var func = allTests[ testNum ];
+      var pauseLength = func();
+      if( pauseLength == -1 ) {
+        return( false );
+      };
+      pauser = setTimeout(
+        function() {
+          runAllTests1( allTests, testNum + 1 );
+        },
+        pauseLength
+      ); 
+    };
+    // Give subscribe enough time to work, then run tests
+    setTimeout(
+      function() {
+        runAllTests1( allTests, startTestNum );
+      },
+      1000
+    );
+  };
+
+/*
+// CODE TO INITIALIZE CLIENT FOR UNIT TEST
+  // Set the initial Firebase reference based on whether the client is authenticated or not.
+  // Retrieve the value stored with the key "f30sUserId" in localStorage. This is a value sent 
+  // by the f30s server to the client via Firebase as an authentication token.
+  GLOB.currentUserId = window.localStorage.getItem("trustjarUserId");
+  // If there's already a value for f30sUserId in localStorage:
+  if (GLOB.currentUserId != null) {
+    // Redefine the primary reference to use the f30UserID value as the primary Firebase reference for all 
+    // client-server communications.
+    GLOB.trustjarRef = new Firebase('https://trustjar.firebaseio.com/' + GLOB.currentUserId)
+  // If there's no value, the user has not been authenticated, so we'll create an arbitrary Firebase reference
+  // so the DOM can load.
+  } else {
+    // create an arbitrary reference so the client can initialize. This reference will
+    // be reset once the client is authenticated.
+    GLOB.trustjarRef = new Firebase('https://trustjar.firebaseio.com/placeholder');
+  };
+*/
+
+GLOB.trustjarRef = new Firebase("https://trustjar.firebaseio.com");
+
+/*
+GLOB.trustjarRef.authAnonymously(function(error, authData) {
+  remember: "sessionOnly"
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+*/
+
+// Initialize all tooltips (bootstrap)
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+GLOB.indexSubmitStep1Ref = GLOB.trustjarRef.child('index/client/step1Submit');
+GLOB.indexServerRef = GLOB.trustjarRef.child('index/server');
+GLOB.pageRef = GLOB.trustjarRef.child('global');
+
+
+
+
+// Server sets the page to be displayed
+GLOB.pageRef.child('currentPage').on('child_added', function(childSnapshot, prevChildName) {
+  // Retrieve the JSON string stored in alertMsg  
+  var val = childSnapshot.val();
+  var curPage = document.location.href.match(/[^\/]+$/)[0]
+  // If a message to change the current page is received, change to that page.  
+  if (curPage != val) {
+    window.location = val;
+  };
+});
+
+// Server changes page to be displayed
+GLOB.pageRef.child('currentPage').on('child_changed', function(childSnapshot, prevChildName) {
+  // Retrieve the JSON string stored in alertMsg  
+  var val = childSnapshot.val();
+  window.location = val;
+  if (curPage != val) {
+    window.location = val;
+  };
+});
+
+
+
+
+
+
+
+
+
+// Add another contact field to the current request form
+function indexSubmitStep1 () {
+      GLOB.indexSubmitStep1Ref.push( {  
+        "userContact" : $('#indexUserContact').val(),
+        "relationshipType" : $("input[name=indexRelationshipType]:checked").val(),
+        "counterpartyContact" : $('#indexCounterpartyContact').val(),
+      } ); 
+      $( "#disableControls" ).addClass( "overlay" );
+      return false; // We don't want the form to trigger a page load. We want to do that through jQuery. 
+}
+
+// Add another contact field to the current request form
+function indexEditStep1 () {
+  $("#indexUserContact").val($("#indexServerUserContact").html());
+  $("#indexCounterpartyContact").val($("#indexServerCounterpartyContact").html());
+  $("#indexCasual").prop( "checked", true );
+  $("#indexCasual").parent('label').addClass('active');
+  $( "#indexStep1" ).show();
+  $( "#step1done" ).hide();
+  $( "#indexStep2" ).hide();
+}
+
+// Add another contact field to the current request form
+GLOB.indexServerRef.on('child_added', function(childSnapshot, prevChildName) {
+  // Retrieve the unique ID from the Firebase message
+  var step1Data = childSnapshot.val();
+  $("#indexServerUserContact").html(step1Data.indexUserContactData);
+  $("#indexServerUserContact2").html(step1Data.indexUserContactData);
+  $("#indexServerRelationshipType").html(step1Data.indexRelationshipTypeData);
+  $("#indexServerCounterpartyContact").html(step1Data.indexCounterpartyContactData);
+  $( "#indexStep1" ).hide();
+  $( "#step1done" ).show();
+  $( "#indexStep2" ).show();
+  $( "#disableControls" ).removeClass( "overlay" );
+});
+
+
+// Add another contact field to the current request form
+GLOB.indexServerRef.on('child_changed', function(childSnapshot, prevChildName) {
+  // Retrieve the unique ID from the Firebase message
+  var step1Data = childSnapshot.val();
+  $("#indexServerUserContact").html(step1Data.indexUserContactData);
+  $("#indexServerUserContact2").html(step1Data.indexUserContactData);
+  $("#indexServerRelationshipType").html(step1Data.indexRelationshipTypeData);
+  $("#indexServerCounterpartyContact").html(step1Data.indexCounterpartyContactData);
+  $( "#indexStep1" ).hide();
+  $( "#step1done" ).show();
+  $( "#indexStep2" ).show();
+  $( "#disableControls" ).removeClass( "overlay" );
+});
+
+
+
+
+
+
+
+
+
+
 
 // Add another contact field to the current request form
 function addProfileContact () {
@@ -12,7 +189,7 @@ function addProfileContact () {
 function addContact () {
   $( "#addContactDash" ).before( "<div class='form-group' style='margin-bottom:8px'><input type='email' class='form-control' id='newContact1' placeholder='phone or email'></div>" );
 }
-
+/*
 // Add another contact field to the current request form
 function indexEditStep1 () {
   $( "#indexStep1" ).show();
@@ -30,6 +207,8 @@ function indexShowStep2 () {
   $( "#step1done" ).show();
   $( "#indexStep2" ).show();
 }
+*/
+
 // Add another contact field to the current request form
 function forgotShowStep2 () {
   $( "#forgotStep1" ).hide();
@@ -80,3 +259,19 @@ function goToDashboard () {
 
 
 // $(".footerContent").get("./footer.html"); 
+
+
+
+
+
+/*
+    function initialize() {
+      // Remove any previous listeners
+      GLOB.trustjarRef.off();
+      GLOB.trustjarRef.remove();
+
+    }
+
+    window.onload = initialize();
+*/
+
